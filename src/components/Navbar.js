e00fe0513c85656dcec32cfac31f0a27a7519de1
mby444 <bimayudha161@gmail.com>
2023-07-15 18:46:21 +0700

    Replace 'import' with 'use' syntax

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,13 @@ import "../styles/Navbar.scss";
 export default function Navbar() {
   return (
     <div className="navbar-container">
-      <Link to="/">
-        <h1>Forum Random</h1>
-      </Link>
+      <div className="title-container">
+          <h1><Link to="/" className="title-link">Forum Random</Link></h1>
+      </div>
+      <div className="sign-btn-container">
+        <Link to="/signup" className="sign-btn signup-btn">Daftar</Link>
+        <Link to="/login" className="sign-btn login-btn">Masuk</Link>
+      </div>
     </div>
   );
 }
